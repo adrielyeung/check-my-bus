2da04b45f45c244c80c05654e646c66421b8cc52
Adriel Yeung <adrielyeung@gmail.com>
2022-11-04 12:56:00 +0800

    Add InputMethodManager import

--- app/src/main/java/com/adriel/checkmybus/MainActivity.java
+++ app/src/main/java/com/adriel/checkmybus/MainActivity.java
@@ -3,6 +3,7 @@ package com.adriel.checkmybus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
